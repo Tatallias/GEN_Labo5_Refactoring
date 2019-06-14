c8dde59ae3387b3d08d90277c85b97162708478c
Claude-André <claudeuw@hotmail.com>
2019-06-14 15:57:10 +0200

    customer integration test for statement

--- test/CustomerTest.cpp
+++ test/CustomerTest.cpp
@@ -8,3 +8,17 @@ TEST(CustomerTest, constructor) {
 
    ASSERT_EQ(cust.getName(), "Violette");
 }
+TEST(CustomerTest, statement) {
+    // code
+    Customer customer("Olivier");
+    customer.addRental( Rental( Movie("Karate Kid"), 7));
+    customer.addRental( Rental( Movie( "Avengers: Endgame", Movie::NEW_RELEASE ), 5));
+    customer.addRental( Rental( Movie("Snow White", Movie::CHILDRENS), 3 ));
+
+    ASSERT_EQ(customer.statement(), "Rental Record for Olivier\n"
+                                    "\tKarate Kid\t9.5\n"
+                                    "\tAvengers: Endgame\t15\n"
+                                    "\tSnow White\t1.5\n"
+                                    "Amount owed is 26\n"
+                                    "You earned 4 frequent renter points");
+}
