72b17ec4173df6dd2d06eae02aad08863e081352
Jorge <jorge.fulgencioesteves@heig-vd.ch>
2019-06-16 23:52:20 +0200

    Added test for Rental new methods

--- test/RentalTest.cpp
+++ test/RentalTest.cpp
@@ -2,6 +2,7 @@
 #include "gtest/gtest.h"
 #include "../src/Rental.h"
 #include "../src/Childrens.h"
+#include "../src/NewRelease.h"
 
 TEST(RentalTest, constructor) {
     // Code
@@ -10,4 +11,32 @@ TEST(RentalTest, constructor) {
 
     ASSERT_EQ(rent.getDaysRented(), 6);
     ASSERT_EQ(rent.getMovie().getTitle(), movie.getTitle());
+}
+
+TEST(RentalTest, getTotal) {
+    // Code
+    Movie movie("Star Wars 21", NewRelease::getNewReleasePriceCode());
+    Movie movie2("Pulpe Fiction");
+    Movie movie3("Roi Lion", Childrens::getChildrensPriceCode());
+    Rental rentNew(movie, 6);
+    Rental rentRegular(movie2, 5);
+    Rental rentChild(movie3, 4);
+
+    ASSERT_EQ(rentNew.getTotal(), 6 * 3);
+    ASSERT_EQ(rentRegular.getTotal(), 6.5);
+    ASSERT_EQ(rentChild.getTotal(), 3);
+}
+
+TEST(RentalTest, getRenterPoints) {
+    Movie movie("Star Wars 21", NewRelease::getNewReleasePriceCode());
+    Movie movie2("Pulpe Fiction");
+    Movie movie3("Roi Lion", Childrens::getChildrensPriceCode());
+    Rental rentNew(movie, 6);
+    Rental rentRegular(movie2, 5);
+    Rental rentChild(movie3, 4);
+
+
+    ASSERT_EQ(rentNew.getRenterPoints(), 2);
+    ASSERT_EQ(rentRegular.getRenterPoints(), 1);
+    ASSERT_EQ(rentChild.getRenterPoints(), 1);
 }
